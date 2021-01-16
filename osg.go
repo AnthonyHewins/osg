872c3bdd29023384d7aec8451d6e28df23a66e33
anthony <aahewins@protonmail.com>
2021-01-16 14:01:15 -0500

    cli changes

--- osg.go
+++ osg.go
@@ -7,37 +7,25 @@ import (
 	"os"
 )
 
-// Commandline options
-type config struct {
-	Mode   string `opts:"mode=arg,  help=One of {remote-zip remote-tar zip tar dir github}"`
-	Arg    string `opts:"mode=arg,  help=The path or URL or github username"`
-
-	Repo   string `opts:"mode=flag, help=The repo you'd like to audit in "`
-	Branch string `opts:"mode=flag, help=The branch you'd like to audit (default is master)"`
-}
-
 func main() {
 	switch len(os.Args) {
 	case 1:
-		fmt.Println("not enough args")
-		help()
-		os.Exit(1)
+		help(1, "not enough args")
 	case 2:
-		if os.Args[1] != "h" || os.Args[1] != "help" {
-			unrecognized(os.Args[1])
-		} else {
-			help()
+		switch os.Args[1] {
+		case "-h", "h", "--help", "help":
+			help(0)
+		default:
+			help(1, "don't understand mode:", os.Args[1])
 		}
 	case 3:
-		normal_audit(os.Args[1], os.Args[2] )
+		normal_audit(os.Args[1], os.Args[2])
 	case 4:
 		github_audit(os.Args[1], os.Args[2], os.Args[3], nil)
 	case 5:
 		github_audit(os.Args[1], os.Args[2], os.Args[3], &os.Args[4])
 	default:
-		fmt.Println("too many args")
-		help()
-		os.Exit(1)
+		help(1, "wrong number of args")
 	}
 }
 
@@ -55,16 +43,13 @@ func normal_audit(mode, arg string) {
 	case "remote-tar", "rt":
 		fn = fetch.RemoteRepoTar
 	case "github", "g":
-		fmt.Println("not enough args for github mode")
-		help()
-		os.Exit(1)
+		help(1, "not enough args for github mode")
 	default:
-		unrecognized(mode)
+		help(1, "don't understand mode:", mode)
 	}
 
 	if arg == "" {
-		fmt.Println("Missing ARG: should be a URL or path")
-		os.Exit(1)
+		help(1, "Missing ARG: should be a URL or path")
 	}
 
 	files := make(chan fetch.Option)
@@ -74,7 +59,7 @@ func normal_audit(mode, arg string) {
 
 func github_audit(mode, username, repo string, branch *string) {
 	if mode != "github" && mode != "g" {
-		unrecognized(mode)
+		help(1, "unrecognized mode:", mode)
 	}
 
 	files := make(chan fetch.Option)
@@ -96,27 +81,26 @@ func start_audit(files chan fetch.Option) {
 	}
 }
 
-func unrecognized(arg string) {
-	fmt.Printf("unrecognized arg: %v\n", arg)
-	help()
-	os.Exit(1)
+func help(exitCode int, extraMessages ...interface{}) {
+	if len(extraMessages) != 0 {
+		fmt.Println(extraMessages...)
+	}
+	fmt.Println(helpText)
+	os.Exit(exitCode)
 }
 
-func help() {
-	fmt.Println(
-		"osg MODE ARG1 ARG2 [...ARG3]\n\n",
-		"MODES             ARGS               DESCRIPTION\n",
-		"-----------------------------------------------------------------------------\n",
-		"help                                 Show this help text\n\n",
+const helpText =
+`osg MODE ARG1 ARG2 [...ARG3]
 
-		"dir, d            REPO               audit local directory REPO\n",
-		"zip, z            REPO.zip           audit local zip file REPO.zip\n",
-		"tar, t            REPO.tar           audit local tar file REPO.tar\n\n",
+MODES             ARGS                 DESCRIPTION
+-------------------------------------------------------------------------------
+help                                   Show this help text
 
-		"remote-zip, rz    URL                audit an arbitrary ZIP file at URL\n",
-		"remote-tar, rt    URL                audit an arbitrary TAR file at URL\n\n",
+dir, d            REPO                 audit local directory REPO
+zip, z            REPO.zip             audit local zip file REPO.zip
+tar, t            REPO.tar             audit local tar file REPO.tar
 
-		"github, g         USER REPO [BRANCH] audit a github repo using the github API\n",
-		"                                     if BRANCH isn't specified 'master' is used\n",
-	)
-}
+remote-zip, rz    URL                  audit an arbitrary ZIP file at URL
+remote-tar, rt    URL                  audit an arbitrary TAR file at URL
+
+github, g         USER REPO [BRANCH]   audit a github repo using the github API`
